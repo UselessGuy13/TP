786e1c88af90730d817c027a46fccc9a61007ecf
UselessGuy13 <47676925+UselessGuy13@users.noreply.github.com>
2019-03-18 20:27:04 -0300

    Add files via upload

---
+++ Loja/src/loja/Carro.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loja;
+
+/**
+ *
+ * @author ETECIA
+ */
+public class Carro {
+    String nome;
+    String marca;
+    String cor;
+    String fabricacao;
+    String largura;
+    String peso;
+    
+    
+    public void acelerar(){
+       System.out.println("Carro acelerando...");
+    }
+    
+    public void acionarTracao(){
+    
+    }
+
+    public void freiar(){
+    
+    }
+    
+    public void acionarSeta(){
+    
+    }
+}
---
+++ Loja/src/loja/ClassePrincipal.java
@@ -0,0 +1,18 @@
+package loja;
+import java.util.Scanner;
+
+public class ClassePrincipal  {
+
+    public static void main(String[] args) {
+
+    Carro obj_carro;
+    obj_carro = new Carro();
+    
+    Carro obj_corsa; 
+    obj_corsa = new Carro();
+    
+    Carro obj_palio;
+    obj_palio = new Carro();
+    }
+
+}
